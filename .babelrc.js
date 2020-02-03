8f124fe2be66b1e2d38d3844ca21ebf1221e56c4
Anton Gilgur <agilgur5@gmail.com>
2020-02-03 16:49:31 -0500

    (refactor): get tsdx test working with babel-jest, JS/JSX
    - TSDX changes some of Jest's defaults and so causes some issues for
      JS/JSX support
      - by default Jest supports JS and JSX in testMatch and babel-jest is
        automatically run on JS/JSX files if a babelrc is found
    
    - also had to configure class-properties for Babel as while it's
      automatically included with tsdx build, no Babel transforms are
      included with tsdx test (yet)
    
    (deps): remove jest as it's a dep of TSDX
    (deps): remove babel-jest@23 since we can use @24 now
    - which is a dep of jest@24
    - remove require.resolve from it as it causes errors due to not being
      a direct dependency

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,8 @@ module.exports = {
   presets: [
     ['@babel/preset-env'], // needed at least until https://github.com/jaredpalmer/tsdx/pull/473 is released
     ['@babel/preset-react']
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties'] // required for tsdx test, related to https://github.com/jaredpalmer/tsdx/issues/383#issuecomment-581637869
   ]
 }
--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,11 @@ module.exports = {
     '<rootDir>/test/config/window-resizeTo.js'
   ],
   transform: {
-    // use babel-jest@23 for babel@6 support (https://github.com/facebook/jest/issues/8230#issuecomment-479470547)
-    '\\.js$': require.resolve('babel-jest')
+    // support babel-jest. TSDX defaults to just ts-jest. see https://github.com/jaredpalmer/tsdx/pull/486
+    '\\.js$': 'babel-jest'
   },
+  // support JS + JSX. TSDX defaults to just TS + TSX. see https://github.com/jaredpalmer/tsdx/pull/486
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   coveragePathIgnorePatterns: [
     '/node_modules/', // default
     '<rootDir>/test/' // ignore any test helper files
