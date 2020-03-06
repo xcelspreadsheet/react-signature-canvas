6a575dcd94e10a063ac8added5f044f4adfa9552
Anton Gilgur <agilgur5@gmail.com>
2020-03-06 07:43:36 -0500

    (deps): move the window.resizeTo polyfill to its own package
    - and import & use it here!

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // configure enzyme w/ react adapter
     '<rootDir>/test/config/configure-enzyme.js',
     // polyfill window.resizeTo
-    '<rootDir>/test/config/window-resizeTo.js'
+    'window-resizeto/polyfill'
   ],
   transform: {
     // use babel-jest@23 for babel@6 support (https://github.com/facebook/jest/issues/8230#issuecomment-479470547)
